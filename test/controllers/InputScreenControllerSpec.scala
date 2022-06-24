8417fcf5c3a1dca2f6d7ba9cd31b0bb4e284fc82
adamjw3 <adam.wright90@yahoo.co.uk>
2022-06-24 15:43:09 +0100

    MRC-85 Add check for when user in db and update bad request to look for error message in results

--- test/controllers/InputScreenControllerSpec.scala
+++ test/controllers/InputScreenControllerSpec.scala
@@ -18,12 +18,12 @@ package controllers
 
 import base.SpecBase
 import forms.InputScreenForm
-import models.NormalMode
+import models.{NormalMode, UserAnswers}
 import org.mockito.MockitoSugar
 import pages.InputScreenPage
 import play.api.mvc.AnyContentAsFormUrlEncoded
-import play.api.test.Helpers.{ POST, status, _ }
-import play.api.test.{ FakeHeaders, FakeRequest }
+import play.api.test.Helpers.{POST, status, _}
+import play.api.test.{FakeHeaders, FakeRequest}
 import repositories.SessionRepository
 import uk.gov.hmrc.http.SessionKeys
 
@@ -134,9 +134,9 @@ class InputScreenControllerSpec extends SpecBase with MockitoSugar {
                 "accountingPeriodStartDate.day"   -> Seq("a"),
                 "accountingPeriodStartDate.month" -> Seq("b"),
                 "accountingPeriodStartDate.year"  -> Seq("c"),
-                "profit"                          -> Seq("a"),
-                "distribution"                    -> Seq("b"),
-                "associatedCompanies"             -> Seq("c")
+                "profit"                          -> Seq("1"),
+                "distribution"                    -> Seq("1"),
+                "associatedCompanies"             -> Seq("1")
               )
             )
           )
@@ -144,6 +144,50 @@ class InputScreenControllerSpec extends SpecBase with MockitoSugar {
           val result = route(application, request).value
 
           status(result) mustEqual 400
+          contentAsString(result) must include("Enter a real Accounting Period Start date");
+        }
+      }
+      "when user already in database" in {
+
+        val application = applicationBuilder(userAnswers = Some(UserAnswers("test-session-id"))).build()
+
+        running(application) {
+          val request = FakeRequest(
+            method = POST,
+            uri = routes.InputScreenController.onSubmit(NormalMode).url,
+            headers = FakeHeaders(Seq.empty),
+            body = AnyContentAsFormUrlEncoded(
+              Map(
+                "accountingPeriodStartDate.day"   -> Seq(epoch.getDayOfMonth.toString),
+                "accountingPeriodStartDate.month" -> Seq(epoch.getMonth.getValue.toString),
+                "accountingPeriodStartDate.year"  -> Seq(epoch.getYear.toString),
+                "profit"                          -> Seq("11"),
+                "distribution"                    -> Seq("22"),
+                "associatedCompanies"             -> Seq("33")
+              )
+            )
+          ).withSession(SessionKeys.sessionId -> "test-session-id")
+
+          val sessionRepository = application.injector.instanceOf[SessionRepository]
+          val result = route(application, request).value
+
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result) must be(Some("/marginal-relief-calculator-frontend/results-page"))
+          sessionRepository
+            .get("test-session-id")
+            .futureValue
+            .get
+            .data
+            .value(InputScreenPage.toString)
+            .as[InputScreenForm] must be(
+            InputScreenForm(
+              accountingPeriodStartDate = epoch,
+              accountingPeriodEndDate = Some(epoch.plusYears(1).minusDays(1)),
+              profit = 11,
+              distribution = 22,
+              associatedCompanies = 33
+            )
+          )
         }
       }
     }
