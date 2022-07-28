3c6108b4a2297c09338874dac303755b32bcf1d2
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2022-07-28 17:07:23 +0100

    MRC-115 Import clean-up

--- test/controllers/AccountingPeriodControllerSpec.scala
+++ test/controllers/AccountingPeriodControllerSpec.scala
@@ -25,7 +25,6 @@ import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
 import pages.{ AccountingPeriodPage, AssociatedCompaniesPage, DistributionPage, DistributionsIncludedPage, TaxableProfitPage }
 import play.api.inject.bind
-import play.api.libs.json.{ JsObject, Json }
 import play.api.mvc.{ AnyContentAsEmpty, AnyContentAsFormUrlEncoded, Call }
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
