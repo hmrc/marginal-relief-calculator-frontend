bca2ca678c28c2164a279b12568c595738f44e78
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2022-08-04 13:39:22 +0100

    MRC-130 Refactored errors based on requirements.

--- app/forms/AssociatedCompaniesFormProvider.scala
+++ app/forms/AssociatedCompaniesFormProvider.scala
@@ -37,21 +37,21 @@ class AssociatedCompaniesFormProvider @Inject() extends Mappings {
             "associatedCompaniesCount.error.required",
             "associatedCompaniesCount.error.wholeNumber",
             "associatedCompaniesCount.error.nonNumeric"
-          ).verifying(minimumValue(1, "error.lessThanOne"), maximumValue(99, "error.greaterThan"))
+          ).verifying(minimumValue(1, "error.lessThanOne"), maximumValue(99, "error.greaterThan99"))
         ),
         "associatedCompaniesFY1Count" -> optional(
           int(
             "associatedCompaniesCount.error.required",
             "associatedCompaniesCount.error.wholeNumber",
             "associatedCompaniesCount.error.nonNumeric"
-          ).verifying(minimumValue(1, "error.lessThanOne"), maximumValue(99, "error.greaterThan"))
+          ).verifying(minimumValue(1, "error.lessThanOne"), maximumValue(99, "error.greaterThan99"))
         ),
         "associatedCompaniesFY2Count" -> optional(
           int(
             "associatedCompaniesCount.error.required",
             "associatedCompaniesCount.error.wholeNumber",
             "associatedCompaniesCount.error.nonNumeric"
-          ).verifying(minimumValue(1, "error.lessThanOne"), maximumValue(99, "error.greaterThan"))
+          ).verifying(minimumValue(1, "error.lessThanOne"), maximumValue(99, "error.greaterThan99"))
         )
       )(AssociatedCompaniesForm.apply)(AssociatedCompaniesForm.unapply)
     }
