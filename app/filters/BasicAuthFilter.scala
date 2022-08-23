02c16e01c8a45e7178dcb073183633ff995f8cd3
Jake-Raffe <jakeraffe24@gmail.com>
2022-08-23 14:37:06 +0100

    Add basic authentication filter

---
+++ app/filters/BasicAuthFilter.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package filters
+
+import akka.stream.Materializer
+import com.google.inject.Inject
+import config.FrontendAppConfig
+import play.api.http.HeaderNames
+import play.api.mvc.{Filter, RequestHeader, Result, Results}
+
+import java.util.Base64
+import scala.concurrent.Future
+
+class BasicAuthFilter @Inject() (appConfig: FrontendAppConfig, override val mat: Materializer) extends Filter {
+
+  private val unauthorized: Result =
+    Results.Unauthorized.withHeaders(HeaderNames.WWW_AUTHENTICATE -> s"""Basic realm="${appConfig.basicAuthRealm}"""")
+
+  override def apply(f: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = {
+    if (appConfig.authEnabled) {
+      println("------------ filter is enabled -------------")
+      rh.headers.get(HeaderNames.AUTHORIZATION) map { authHeader =>
+        val (user, pass) = decodeBasicAuth(authHeader)
+        println(user + "--------" + pass)
+        if (appConfig.basicAuthUser.contains(user) && appConfig.basicAuthPassword.contains(pass)) {
+          println("------------ passed -------------")
+          f(rh)
+        } else {
+          println("------------ failed -------------")
+          Future.successful(unauthorized)
+        }
+      } getOrElse Future.successful(unauthorized)
+    } else {
+      f(rh)
+    }
+  }
+
+//  private[this]
+  def decodeBasicAuth(authHeader: String): (String, String) = {
+    val authBasicValue = authHeader.replaceFirst("Basic ", "")
+    val decoded = Base64.getDecoder.decode(authBasicValue)
+    val Array(user, password) = new String(decoded).split(":")
+    (user, password)
+  }
+}
