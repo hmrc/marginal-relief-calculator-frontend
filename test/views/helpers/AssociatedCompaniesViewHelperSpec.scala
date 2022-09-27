bb3fe0e9e2678bdd38a3c59b6cf225022a1f2ba0
girishkamat <girishkamat@users.noreply.github.com>
2022-09-27 15:40:45 +0100

    MRC-187 Improved coverage

---
+++ test/views/helpers/AssociatedCompaniesViewHelperSpec.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.helpers
+
+import connectors.sharedmodel.{ AskBothParts, AskFull, AskOnePart, Period }
+import forms.AssociatedCompaniesFormProvider
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import play.api.i18n.Messages
+import play.api.test.Helpers
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.html.components.{ GovukErrorMessage, GovukHint, GovukInput, GovukLabel }
+import uk.gov.hmrc.govukfrontend.views.viewmodels.label.Label
+import viewmodels.govuk.input.{ InputViewModel, _ }
+
+import java.time.LocalDate
+
+class AssociatedCompaniesViewHelperSpec extends AnyFreeSpec with Matchers with ViewHelper {
+  private implicit val messages: Messages = Helpers.stubMessages(messagesApi =
+    Helpers.stubMessagesApi(
+      messages = Map(
+        "en" -> Map(
+          "associatedCompanies.countLabel"      -> "Associated companies count",
+          "associatedCompanies.heading"         -> "Associated companies",
+          "associatedCompanies.heading.between" -> "Associated companies between {0} and {1}"
+        )
+      )
+    )
+  )
+  val formProvider = new AssociatedCompaniesFormProvider()
+
+  private val govukErrorMessage = new GovukErrorMessage
+  private val govukHint = new GovukHint
+  private val govukLabel = new GovukLabel
+  private val govukInput = new GovukInput(govukErrorMessage, govukHint, govukLabel)
+
+  "yesHtml" - {
+    "should return input field when AskAssociatedCompaniesParameter is AskFull" in {
+      val form = formProvider.apply()
+      AssociatedCompaniesViewHelper.yesHtml(form, AskFull) shouldBe Some(
+        govukInput(
+          InputViewModel(
+            field = form("associatedCompaniesCount"),
+            label = Label(content = Text(messages("associatedCompanies.countLabel")))
+          )
+            .withCssClass("govuk-!-width-one-third")
+            .withAttribute("maxlength" -> "2")
+        )
+      )
+    }
+
+    "should return input field when AskAssociatedCompaniesParameter is AskOnePart" in {
+      val form = formProvider.apply()
+      AssociatedCompaniesViewHelper.yesHtml(
+        form,
+        AskOnePart(Period(LocalDate.ofEpochDay(0), LocalDate.ofEpochDay(1)))
+      ) shouldBe Some(
+        govukInput(
+          InputViewModel(
+            field = form("associatedCompaniesCount"),
+            label = Label(content = Text("Associated companies count"))
+          )
+            .withCssClass("govuk-!-width-one-third")
+            .withAttribute("maxlength" -> "2")
+        )
+      )
+    }
+
+    "should return None when AskAssociatedCompaniesParameter is DontAsk" in {
+      val form = formProvider.apply()
+      AssociatedCompaniesViewHelper.yesHtml(
+        form,
+        AskBothParts(
+          Period(LocalDate.ofEpochDay(0), LocalDate.ofEpochDay(1)),
+          Period(LocalDate.ofEpochDay(0), LocalDate.ofEpochDay(1))
+        )
+      ) shouldBe None
+    }
+  }
+
+  "heading" - {
+    "should be Associated companies when AskAssociatedCompaniesParameter is AskFull" in {
+      AssociatedCompaniesViewHelper.heading(AskFull) shouldBe h1("Associated companies", classes = "govuk-heading-l")
+    }
+
+    "should be Associated companies when AskAssociatedCompaniesParameter is AskBothParts" in {
+      AssociatedCompaniesViewHelper.heading(
+        AskBothParts(
+          Period(LocalDate.ofEpochDay(0), LocalDate.ofEpochDay(1)),
+          Period(LocalDate.ofEpochDay(0), LocalDate.ofEpochDay(1))
+        )
+      ) shouldBe h1("Associated companies", classes = "govuk-heading-l")
+    }
+
+    "should be Associated companies when AskAssociatedCompaniesParameter is AskOnePart" in {
+      AssociatedCompaniesViewHelper.heading(
+        AskOnePart(Period(LocalDate.ofEpochDay(0), LocalDate.ofEpochDay(1)))
+      ) shouldBe h1("Associated companies between 1 January 1970 and 2 January 1970", classes = "govuk-heading-l")
+    }
+  }
+}
---
+++ test/views/helpers/ViewHelperSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.helpers
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import play.twirl.api.Html
+
+class ViewHelperSpec extends AnyFreeSpec with Matchers {
+
+  "h1" - {
+    "when style provided, should return HTML with style attribute" in {
+      val viewHelper = new ViewHelper {}
+      viewHelper.h1("some-text", "some-class", "some-style") shouldBe Html(s"""<h1 class="some-class" style="some-style">some-text</h1>""")
+    }
+
+    "when style not provided, should return HTML without style attribute" in {
+      val viewHelper = new ViewHelper {}
+      viewHelper.h1("some-text", "some-class") shouldBe Html(s"""<h1 class="some-class" >some-text</h1>""")
+    }
+  }
+
+  "h2" - {
+    "when style provided, should return HTML with style attribute" in {
+      val viewHelper = new ViewHelper {}
+      viewHelper.h2("some-text", "some-class", "some-style") shouldBe Html(s"""<h2 class="some-class" style="some-style">some-text</h2>""")
+    }
+
+    "when style not provided, should return HTML without style attribute" in {
+      val viewHelper = new ViewHelper {}
+      viewHelper.h2("some-text", "some-class") shouldBe Html(s"""<h2 class="some-class" >some-text</h2>""")
+    }
+  }
+}
