22ca1966a7868b40d5947cd3a27b84a29c5fd26f
adamjw3 <367147+adamjw3@users.noreply.github.com>
2022-10-03 15:47:33 +0100

    MRC-187 Add table header text

--- app/views/helpers/FullResultsPageHelper.scala
+++ app/views/helpers/FullResultsPageHelper.scala
@@ -318,8 +318,7 @@ object FullResultsPageHelper extends ViewHelper {
               classes = "not-header"
             ),
             HeadCell(
-              content = HtmlContent(s"""<span class="govuk-!-display-none">No header</span>"""),
-              classes = "not-header"
+              content = HtmlContent(s"""<span class="govuk-!-display-none">Calculation variables</span>"""),
             ),
             HeadCell(content = Text(messages("fullResultsPage.calculation"))),
             HeadCell(content = Text(messages("fullResultsPage.result")))
