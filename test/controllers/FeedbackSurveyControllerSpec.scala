7bf2f94a67c56c93c172ed40e36d3263a078d53f
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2022-12-01 10:24:24 +0000

    MRC-249: test fix

--- test/controllers/FeedbackSurveyControllerSpec.scala
+++ test/controllers/FeedbackSurveyControllerSpec.scala
@@ -1,33 +1,44 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
-import com.fasterxml.jackson.databind.cfg.ConfigOverride
 import config.FrontendAppConfig
-import filters.BasicAuthFilterSpec.frontendAppConfig
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import org.scalatestplus.mockito.MockitoSugar
-import play.api.Configuration
+import play.api.Application
 import play.api.http.Status
-import play.api.test.Helpers.{redirectLocation, status}
-import play.test.Helpers.fakeRequest
-
-
-class ConfigOverride(config: Configuration) extends FrontendAppConfig(config: Configuration) {
-  override val exitSurveyUrl: String = "/marginal-relief-calculator"
-}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
 
 class FeedbackSurveyControllerSpec extends SpecBase with MockitoSugar {
-  val mockController =  new FeedbackSurveyController(identify = ???, appConfig = ???, controllerComponents = ???){
-    override implicit val appConfig = ConfigOverride
-  }
+  val app: Application = applicationBuilder().build
+  val config: FrontendAppConfig = app.injector.instanceOf[FrontendAppConfig]
 
-  "Feedback Survey Controller" should {
+  "Feedback Survey Controller" - {
 
     "Redirect to the ExitSurvey and clear a session" in {
-      val result = mockController.redirectToExitSurvey()(fakeRequest)
+      running(app) {
+        val request = FakeRequest(GET, routes.FeedbackSurveyController.redirectToExitSurvey.url)
 
-      status(result) mustBe Status.SEE_OTHER
-      redirectLocation(result) mustBe Some(frontendAppConfig.exitSurveyUrl)
+        val result = route(app, request).value
+
+        status(result) mustBe Status.SEE_OTHER
+        redirectLocation(result) mustBe Some(config.exitSurveyUrl)
+      }
     }
   }
 
