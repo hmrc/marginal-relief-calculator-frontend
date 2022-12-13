126a9dada7cadb125ffdd32f336501bfadb20845
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2022-12-13 19:28:55 +0000

    mrc-280: sbt plugin / Dependencies fix.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,6 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.23.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "7.1.0",
-    "uk.gov.hmrc"       %% "play-language"                 % "5.1.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "0.71.0",
     "org.julienrf"      %% "play-json-derived-codecs"      % "10.0.2",
     "org.typelevel"     %% "cats-core"                     % "2.7.0",
