d7c7d07c9254aa4e9c21de1c24c9e903584e6461
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-05-29 16:27:48 +0100

    DLS-10273: Updates Dependencies, adds TODO around removing test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,18 +12,19 @@ object AppDependencies {
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "2.0.0",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % hmrcMongoVersion,
     "com.openhtmltopdf"  % "openhtmltopdf-pdfbox"                        % "1.0.10",
-    "org.julienrf"      %% "play-json-derived-codecs"                    % "10.1.0",
-    "org.typelevel"     %% "cats-core"                                   % "2.10.0"
+    "org.julienrf"      %% "play-json-derived-codecs"                    % "11.0.0",
+    "org.typelevel"     %% "cats-core"                                   % "2.12.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
-    "org.scalacheck"         %% "scalacheck"                    % "1.17.0",
-    "org.scalatestplus"      %% "scalacheck-1-17"               % "3.2.17.0",
-    "org.mockito"            %% "mockito-scala"                 % "1.17.30",
-    "org.scalatestplus"      %% "mockito-4-11"                  % "3.2.17.0",
-    "org.jsoup"               % "jsoup"                         % "1.17.1",
+//    TODO: CHECK IF ANY OF BELOW 4 CAN BE REMOVED
+    "org.scalacheck" %% "scalacheck" % "1.17.0",
+    "org.scalatestplus" %% "scalacheck-1-17" % "3.2.17.0",
+    "org.mockito" %% "mockito-scala" % "1.17.30",
+    "org.scalatestplus" %% "mockito-4-11" % "3.2.17.0",
+    "org.jsoup"               % "jsoup"                         % "1.17.2",
     "org.pegdown"             % "pegdown"                       % "1.6.0",
     "com.softwaremill.diffx" %% "diffx-scalatest-should"        % "0.9.0"
   ).map(_ % "test, it")
