d342f8374f33f1515f4016bd08d8a6cce9972ac5
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-05-29 15:46:03 +0100

    DLS-10273: Tidies up Dependencies and Plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,11 +8,11 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % hmrcMongoVersion,
     "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "8.5.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "2.0.0",
-    "org.julienrf"      %% "play-json-derived-codecs"                    % "10.1.0",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % hmrcMongoVersion,
     "com.openhtmltopdf"  % "openhtmltopdf-pdfbox"                        % "1.0.10",
+    "org.julienrf"      %% "play-json-derived-codecs"                    % "10.1.0",
     "org.typelevel"     %% "cats-core"                                   % "2.10.0"
   )
 
@@ -23,9 +23,9 @@ object AppDependencies {
     "org.scalatestplus"      %% "scalacheck-1-17"               % "3.2.17.0",
     "org.mockito"            %% "mockito-scala"                 % "1.17.30",
     "org.scalatestplus"      %% "mockito-4-11"                  % "3.2.17.0",
+    "org.jsoup"               % "jsoup"                         % "1.17.1",
     "org.pegdown"             % "pegdown"                       % "1.6.0",
-    "com.softwaremill.diffx" %% "diffx-scalatest-should"        % "0.9.0",
-    "org.jsoup"               % "jsoup"                         % "1.17.1"
+    "com.softwaremill.diffx" %% "diffx-scalatest-should"        % "0.9.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
